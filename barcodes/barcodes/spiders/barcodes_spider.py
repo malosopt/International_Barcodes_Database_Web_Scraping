65adb2bf838a42ef435c67e6600d6aa68d0a1df9
Miguel Angel <Miangel00.77@gmail.com>
2021-06-12 17:44:20 -0500

    Fixing Sintax: OK

--- barcodes/barcodes/spiders/barcodes_spider.py
+++ barcodes/barcodes/spiders/barcodes_spider.py
@@ -1,41 +1,43 @@
+# 12/06/2021
+# Miguel Angel Sanchez
+# From COlOMBIA
+# @15novmasr Instagram
+# @15Once94 Twitter
+
 import scrapy
 import re
 
-for i in range(100000,200):
-    print(i)
-
-
 
 class BarcodesSpiderSpider(scrapy.Spider):
+
     name = 'barcodes_spider'
     allowed_domains = ['https://barcodesdatabase.org/']
     
     urls = []
+
     for i in range(0,100000,200): 
         urls.append(f'https://barcodesdatabase.org/selected-products/?pa={i}&sort_options=&country_list_select=')
 
-    start_urls = urls#['https://barcodesdatabase.org/selected-products/']
+    start_urls = urls #['https://barcodesdatabase.org/selected-products/']
 
 
-
-    # Configuraciones
+    #Settings
     custom_settings = {
 
-        'FEED_URI' : 'barcodes10.json', 
-        'FEED_FORMAT' : 'json',
+        'FEED_URI' : 'barcodes.json', # if you want a csv file, put barcodes.json
+        'FEED_FORMAT' : 'json',       # if you want a csv file, just change 'json' by 'csv'
         'FEED_EXPORT_ENCODING': 'utf-8',
-        'CONCURRENT_REQUESTS' : 10 #ScraperAPI  
-        #'RETRY_TIMES': RETRY_TIMES, # ScraperAPI Recommendation
-        #'ROBOTSTXT_OBEY' : False,
-        #'CONCURRENT_REQUESTS_PER_IP' : C_R_PER_IP_PUBLISHER #ScraperAPI  
+        'CONCURRENT_REQUESTS' : 2 , 
+        'RETRY_TIMES': 4 ,
+        'ROBOTSTXT_OBEY' : True 
         
     }
+
 
     def parse(self, response):
 
         barcodes = response.css("tbody#list-products-body tr td a::attr(href)").re('[0-9]{13}')
-        print(barcodes)
+        #print(barcodes)
         yield {
-            'bcode':barcodes
-        }
-
+                'bcode':barcodes
+              }
